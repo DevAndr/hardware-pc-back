be08bee2a4ced671351a31b149c725eff5dfa04d
DevAndr <pitbullstar96@yandex.ru>
2022-05-26 01:33:11 +0300

    nextjs app

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,23 @@ import { useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 import axios from "axios";
 
+export async function getServerSideProps({ req }) {
+	// WARNING: This is a generally unsafe application unless you're deploying to a managed platform like Vercel.
+	// Be sure your load balancer is configured to not allow spoofed host headers.
+	return { props: { host: `${getProtocol(req)}://${req.headers.host}` } }
+}
+
+function getProtocol(req) {
+	if (req.connection.encrypted) {
+		return 'https'
+	}
+	const forwardedProto = req.headers['x-forwarded-proto']
+	if (forwardedProto) {
+		return forwardedProto.split(/\s*,\s*/)[0]
+	}
+	return 'http'
+}
+
 export default function Home() {
 	const [data, setData] = useState([])
 
