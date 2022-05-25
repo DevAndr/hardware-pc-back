da17268abaccac15a2bd12e4f42a333e76fedf7c
DevAndr <pitbullstar96@yandex.ru>
2022-05-26 01:42:43 +0300

    nextjs app

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,23 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react'
-import styles from '../styles/Home.module.css'
-import axios from "axios";
+import useSwr from 'swr'
+import Link from 'next/link'
 
-export async function getServerSideProps({ req }) {
-	// WARNING: This is a generally unsafe application unless you're deploying to a managed platform like Vercel.
-	// Be sure your load balancer is configured to not allow spoofed host headers.
-	return { props: { host: `${getProtocol(req)}://${req.headers.host}` } }
-}
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
-function getProtocol(req) {
-	if (req.connection.encrypted) {
-		return 'https'
-	}
-	const forwardedProto = req.headers['x-forwarded-proto']
-	if (forwardedProto) {
-		return forwardedProto.split(/\s*,\s*/)[0]
-	}
-	return 'http'
-}
+export default function Index() {
+  const { data, error } = useSwr('/api/users', fetcher)
 
-export default function Home() {
-	const [data, setData] = useState([])
+  if (error) return <div>Failed to load users</div>
+  if (!data) return <div>Loading...</div>
 
-	useEffect(() => {
-		fetchData();
-	}, [])
-
-	async function fetchData() {
-		const res = await axios('/api/test');
-		setData(res.data)
-		console.log(res.data)
-	}
-
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<div>Hello</div>
-
-			<div className={styles.inputArea}>
-				{
-					JSON.stringify(data)
-				}
-			</div>
-		</div>
-	)
+  return (
+    <ul>
+      {data.map((user) => (
+        <li key={user.id}>
+          <Link href="/user/[id]" as={`/user/${user.id}`}>
+            <a>{`User ${user.id}`}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
--- routes/buildDataParser.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router;
-const {parsingData} = require("../controllers/parserController");
-
-const routerParser = new Router();
-const ROUTE_NAME = "parser";
-
-routerParser.get(`/${ROUTE_NAME}`, parsingData)
-
-module.exports = routerParser
