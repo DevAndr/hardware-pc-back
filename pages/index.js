14183b6ad48727dcb74e2700a1a1f20887327cec
DevAndr <pitbullstar96@yandex.ru>
2022-05-26 01:57:42 +0300

    nextjs app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import useSwr from 'swr'
 import Link from 'next/link'
+import axios from "axios";
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetchData = async() => {
+  const res = await axios('/api/test');
+  return res.data;
+  // setData(res.data)
+  // console.log(res.data)
+}
 
 export default function Index() {
   const { data, error } = useSwr('/api/users', fetcher)
+  const { hw, errorHw } = useSwr('/api/users', fetchData)
 
   if (error) return <div>Failed to load users</div>
   if (!data) return <div>Loading...</div>
@@ -18,6 +26,9 @@ export default function Index() {
           </Link>
         </li>
       ))}
+      {
+        JSON.stringify(hw)
+      }
     </ul>
   )
 }
