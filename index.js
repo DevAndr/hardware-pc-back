d44c58e672ffcc642add1261eec7306aca33d9dd
DevAndr <pitbullstar96@yandex.ru>
2022-05-25 14:42:46 +0300

    delete cors

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const cors = require('cors')
 const routerParser = require('./routes/buildDataParser')
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 
-app.get('api/', routerParser)
-app.get('api/test', (req, res) => {
+app.get('/', routerParser)
+app.get('/test', async (req, res) => {
     res.json({data: "ok"})
 })
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
     console.log('server started')
-
 })
